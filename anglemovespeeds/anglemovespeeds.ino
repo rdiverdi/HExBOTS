8fcc3df4c1ebea72710680011b6a855661f10a9d
Rebecca Jordan <rebecca.jordan@students.olin.edu>
2015-10-22 21:22:49 -0400

    attempt at matrix multiplication.

--- anglemovespeeds/anglemovespeeds.ino
+++ anglemovespeeds/anglemovespeeds.ino
@@ -1,23 +1,32 @@
 #include <MatrixMath.h>
+float speeds[3]; 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
+void turnspeeds(float *movespds, int d, int theta, int gamma, float *anglespds) //function that doesn't return anything but edits the array
+  {
+    
+    float matrix[3][3] =
+    {
+      {cos(gamma)*cos(theta), d*sin(gamma)*sin(theta), d*sin(gamma)*cos(theta)},
+      {cos(gamma)*sin(theta), d*sin(gamma)*cos(theta), d*sin(gamma)*sin(theta)},
+      {-sin(gamma), 0, -d*cos(gamma)}  
+    };
+    float dthetagamma[3];
+    Matrix.Invert((float*)matrix, 3);
+    Matrix.Multiply((float*)matrix, (float*)movespds, 1, 3, 3, (float*)dthetagamma);
+    Matrix.Print((float*)matrix, 3, 3, "matrix"); //just print that now-inverted matrix
+    Matrix.Print((float*)dthetagamma, 1, 3, "dthetagamma");
+  }
+
 void loop() {
   // put your main code here, to run repeatedly:
-  byte anglespeeds[3]; //initialize a byte array of size 3?
-
-  void turnspeeds(int vx, int vy, int vz, int d, int theta, int gamma, byte *speeds) //function that doesn't return anything but edits the array
-  {
-    int matrix[3][3] =
-    {
-      {1, 2, 3}
-      {4, 5, 6}
-      {7, 8, 9}  
-    };
-    Matrix.Print((float*)matrix, 3, 3, "matrix"); //just print that matrix
-
-  }
+  float anglespeeds[3]; //intialize a 3x1 array to store speeds of d, theta, gamma
+  float movespeeds[3] = {0, 1, 0}; //initialize a 3x1 array to store desired x, y, and z speeds
+  
+  turnspeeds(movespeeds, 1, 1, 1, anglespeeds);
+  
 }
