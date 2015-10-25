c05796d9370e3352017a109ef9f458f3b3a503d0
jaycesheachow <jayce.chow@students.olin.edu>
2015-10-25 16:05:06 -0400

    Comments and Ramblings
    Ignore for the most part.

--- Trig.m
+++ Trig.m
@@ -20,9 +20,9 @@ for xpos = 1:xinc
             square = sqrt((xpos-(L0*cos(tha)))^2 +...
                 (ypos-(L0*sin(tha)))^2 + (zpos^2));
             if square <= (L1+L2)
-                table(xpos, ypos, zpos,1) = tha;
-                table(xpos, ypos, zpos,2) = phi;
-                table(xpos, ypos, zpos,3) = psi;
+                table(xpos, ypos, zpos,1) = tha*180/pi;
+                table(xpos, ypos, zpos,2) = phi*180/pi;
+                table(xpos, ypos, zpos,3) = psi*180/pi;
                 n = n+1;
             end
         end
@@ -31,7 +31,8 @@ for xpos = 1:xinc
 end
 
 table
-n
+% n
+% PLOTTING STUFF
 % [tha;phi;psi]
 
 % x1 = L1*cos(phi);
@@ -48,3 +49,17 @@ n
 % plot3([0, x1,x2],[0,y1,y2],[0, z1,z2],'rp-')
 % 
 % sqrt((x2)^2+(z2)^2)
+
+%% Notes and Thoughts
+% Currently, the table will tell the leg how to move from 0,0,0 to a
+% certain forward point. For going in reverse, we need to ask it to reset
+% its values to "zero", move twice the distance along the ground while
+% having the servos spin backwards. Then those will rezero and move up and
+% forward. All the while, the opposite set of legs is moving the same at a
+% time offset. 
+
+% Also, Matlab indexes everything from 1, and the servo and positions need
+% to all be from 0.
+
+% One last thing, I think we should subtract 90 degrees from all the
+% values, unless servoes like them more like this. I don't know...
