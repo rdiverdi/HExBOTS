6c68287f856abd066d1686d580ba8b3a992dc429
Rebecca Jordan <rebecca.jordan@students.olin.edu>
2015-12-14 22:37:56 -0500

    code for walking based on velocity

--- hexbot_pkg/nodes/HExBOT_move.py
+++ hexbot_pkg/nodes/HExBOT_move.py
@@ -10,19 +10,21 @@ from Leg import *
 # index of array is the timestep
 # element of array is [x, y, z] position of leg relative to leg origin
 # for legs touching ground (repeats because some legs will do things on 1st timestep and some will do on the 25th.)
-walkdown = [None]*48
-walkup = [None]*48
+walkdown = [None] * 48
+walkup = [None] * 48
 for i in range(0, 24):
-    walkdown[i] = [6, 1.8 - (i*3.6/23), -4]
+    walkdown[i] = [6, 1.8 - (i * 3.6 / 23), -4]
     walkdown[i + 24] = walkdown[i]
-    walkup[i] = ([6, -1.8 + (i*3.6/23), -abs(i - 11)/3.0])
+    walkup[i] = ([6, -1.8 + (i * 3.6 / 23), -abs(i - 11) / 3.0])
     walkup[i + 24] = walkup[i]
+
 
 def init_legs():
     legs = []
-    for i in range(0,6):
-        legs.append(Leg(i+1))
+    for i in range(0, 6):
+        legs.append(Leg(i + 1))
     return legs
+
 
 def movebot(command, timestep, legs):
     """
@@ -61,12 +63,30 @@ def walkforward(timestep, legs):
             [x, y, z] = walkdown[timestep]
 
         if leg.index < 4:
-            x = -1*x
+            x = -1 * x
         angles = leg.findangles(x, y, z)
         for (i, angle) in enumerate(angles):
-            if leg.even != evensturn and i%3 == 1:
-                angle = angle-18
-            anglelist.append((angle+180)%360-180)
+            if leg.even != evensturn and i % 3 == 1:  # find servo 1 of all legs that are down
+                angle = angle - 18  # move servo 1 an extra 18 degrees down to account for angle loss due to holding weight
+            anglelist.append((angle + 180) % 360 - 180)  # convert angle to -180 to 180
+    return anglelist
+
+
+def walkrate(legs, dx, dy, runrate):
+    """
+    takes in a list of the leg instances
+    walks in dx, dy direction (rates given in inches per second)
+    returns the list of 18 angles for the current time
+    """
+    anglelist = []
+
+    for leg in legs:
+        angles = leg.movespeed(dx, dy, runrate)
+        # add 18 degrees down to servo 1 if the leg should be on the ground 
+        # maybe we should move this to findangles
+        if leg.onground < 1:
+            angles(1) -= 18
+        anglelist.extend(angles)
     return anglelist
 
 # create leg instances for testing. In use, these will be created in ROS_coms file
@@ -75,9 +95,9 @@ if __name__=='__main__':
     for i in range(0, 6):
         legs.append(Leg(i))
 
-    #for i in range(48):
+    # for i in range(48):
     #    movebot('walkforward', i, legs)
-    #print len(walkdown)
-    #for i in range(len(walkdown)):
+    # print len(walkdown)
+    # for i in range(len(walkdown)):
     #    print walkdown[i]
-    #print len(walkforward(7, legs))
+    # print len(walkforward(7, legs))
